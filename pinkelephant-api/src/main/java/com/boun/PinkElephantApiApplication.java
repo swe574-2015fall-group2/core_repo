658c264b7ad5b6acaac00d7b7d33922c673597b2
mehmetcelikel <mehmetcelikel@gmail.com>
2015-10-31 07:49:15 +1100

    Test

--- pinkelephant-api/src/main/java/com/boun/PinkElephantApiApplication.java
+++ pinkelephant-api/src/main/java/com/boun/PinkElephantApiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PinkElephantApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PinkElephantApiApplication.class, args);
+        SpringApplication.run(PinkElephantApiApplication.class, args); 
     }
 }
