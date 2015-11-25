15753447ddb03a93a44f2c3a81c2481a4873daa1
mehmetcelikel <mehmetcelikel@gmail.com>
2015-11-26 10:37:41 +1100

    image base filepath is updated

--- pinkelephant-commons/src/main/java/com/boun/data/common/Constants.java
+++ pinkelephant-commons/src/main/java/com/boun/data/common/Constants.java
@@ -2,5 +2,5 @@ package com.boun.data.common;
 
 public final class Constants {
 
-	public static final String IMAGE_BASE_FILE_PATH = "C:/swe574/images/";
+	public static final String IMAGE_BASE_FILE_PATH = "/swe574/images/";
 }
